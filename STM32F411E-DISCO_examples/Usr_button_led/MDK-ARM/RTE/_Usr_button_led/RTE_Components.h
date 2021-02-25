ac4cd84450221d34c1e6843ef48b07dd092afd94
Strfr <47529454+strfr@users.noreply.github.com>
2021-02-25 14:05:54 +0300

    user button led example for stm32f411

---
+++ STM32F411E-DISCO_examples/Usr_button_led/MDK-ARM/RTE/_Usr_button_led/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Usr_button_led' 
+ * Target:  'Usr_button_led' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
